c26745ac04d0513e46c63c0f9212fe6cd8cf0ab3
Brian&Ian <david+luke@appacademy.io>
2013-04-20 13:29:59 -0700

    Add UI features and game pausing

--- diamondback/diamondback.js
+++ diamondback/diamondback.js
@@ -10,13 +10,13 @@ var Board = function(height, width) {
   this.width = width;
   this.playArea = [];
   this.makePlayArea();
-  this.playArea[13][13] = "CR"; // TEST
-  this.playArea[13][12] = "CR"; // TEST
-  this.playArea[13][11] = "CR"; // TEST
+  this.playArea[13][13] = "s";
+  this.playArea[13][12] = "s";
+  this.playArea[13][11] = "s";
 };
 
 var Snake = function() {
- this.moveDelta = [0,-1];
+ this.moveDelta = [1,0];
  this.corpus = [[13, 13], [13, 12], [13, 11]];
 };
 
@@ -30,23 +30,19 @@ Board.prototype.makePlayArea = function() {
   }
 };
 
-Board.prototype.render = function() {
-  for(i=0; i < this.playArea.length; i++) {
-    console.log(this.playArea[i]);
-  }
-  console.log("_________________________");
-};
-
 Snake.prototype.turn = function (direction) {
-  var legalDirs = {south: [0, 1], north: [0, -1], east: [1, 0], west: [-1, 0]}
-  if(this.validTurn(direction)) {
+  var legalDirs = {south: [1, 0], north: [-1, 0], east: [0, 1], west: [0, -1]};
+
+  if(this.validTurn(direction, legalDirs)) {
     this.moveDelta = legalDirs[direction];
   }
 };
 
-Snake.prototype.validTurn = function(direction) {
-  legalDirs[direction][0] + this.moveDelta[0] !== 0 ||
-  legalDirs[direction][1] + this.moveDelta[1] !== 0;
+Snake.prototype.validTurn = function(direction, legalDirs) {
+  if (legalDirs[direction][0] + this.moveDelta[0] !== 0 ||
+    legalDirs[direction][1] + this.moveDelta[1] !== 0) {
+      return true;
+  }
 };
 
 Game.prototype.step = function() {
@@ -66,59 +62,47 @@ Game.prototype.dropFood = function() {
     var dropYCor = (Math.floor(Math.random() * this.board.height));
     var dropXCor = (Math.floor(Math.random() * this.board.width));
     if(this.board.playArea[dropYCor][dropXCor] == null) {
-      this.board.playArea[dropYCor][dropXCor] = "aa";
+      this.board.playArea[dropYCor][dropXCor] = "f";
     }
   }
 };
 
 Game.prototype.moveSnake = function(newHead) {
-  this.snake.corpus.unshift(newHead);
-  this.board.playArea[newHead[0]][newHead[1]] = "CR"; //TEST
-
-  if (this.board[newHead] == null) {
+  if (this.board.playArea[newHead[0]][newHead[1]] == null) {
     var snakeTail = _.last(this.snake.corpus);
-    this.board.playArea[snakeTail[0]][snakeTail[1]] = null; //TEST
+    this.board.playArea[snakeTail[0]][snakeTail[1]] = null;
     this.snake.corpus.pop();
   }
+
+  this.snake.corpus.unshift(newHead);
+  this.board.playArea[newHead[0]][newHead[1]] = "s";
 };
 
 Game.prototype.dontLoseYourHead = function(newHead) {
-  console.log(newHead);
   if(newHead[0] >= this.board.height) {
-    newHead[0] -= this.board.height
+    newHead[0] -= this.board.height;
   } else if (newHead[0] < 0) {
-    newHead[0] += this.board.height
+    newHead[0] += this.board.height;
   }
   if(newHead[1] >= this.board.width) {
-    newHead[1] -= this.board.width
+    newHead[1] -= this.board.width;
   } else if (newHead[1] < 0) {
-    newHead[1] += this.board.width
+    newHead[1] += this.board.width;
   }
-  console.log(newHead);
 };
 
 Game.prototype.collisionAhead = function(newHead) {
   var that = this;
-  _.contains(that.snake.corpus, newHead);
+
+  console.log("newhead");
+  console.log(newHead);
+
+  console.log("snake corpus");
+  console.log(that.snake.corpus);
+  console.log(_(that.snake.corpus).contains(newHead));
+  return _(that.snake.corpus).contains(newHead);
 };
 
 Game.prototype.youLose = function() {
   console.log("you lose");
 };
-
-// g.step();
-
-
-
-
-// Snake.prototype.move = function () {
-//   newHead = [ this.moveDirs[0] + first(this.corpus)[0],
-//               this.moveDirs[1] + first(this.corpus)[1]  ];
-//
-//   if board.newHead = null
-//   this.corpus.pop;
-//   this.corpus.unshift(newHead);
-// };
-
-
-
--- diamondback/diamondback_ui.js
+++ diamondback/diamondback_ui.js
@@ -1,5 +1,25 @@
 var Controller = function() {
   this.game = new Game();
+  this.paused = false;
+};
+
+Controller.prototype.step = function() {
+    this.game.step();
+    this.render();
+};
+
+Controller.prototype.runLoop = function() {
+  var that = this;
+  var gameLoopId = window.setInterval(function () {
+    that.step();
+    if (that.paused) {
+      clearInterval(gameLoopId);
+    }
+  }, 150);
+};
+
+Controller.prototype.stopLoop = function() {
+  this.paused = true;
 };
 
 Controller.prototype.buildBoard = function() {
@@ -17,33 +37,61 @@ Controller.prototype.render = function() {
   for(i = 0; i < this.game.board.height; i++) {
     for(j = 0; j < this.game.board.width; j++) {
       var classToAdd = "snake";
-+
+
       switch(this.game.board.playArea[i][j]) {
         case null:
           classToAdd = "empty";
           break;
-        case "aa":
+        case "f":
           classToAdd = "fruit";
           break;
-        case "CR":
+        case "s":
           classToAdd = "snake";
           break;
         default:
           classToAdd = "shit";
         }
-      var squareEl = "<td data-row='" + i +
-                       "' data-col='" + j + "'></td>";
+      var squareEl = "[data-row='" + i + "'][data-col='" + j + "']";
 
-
-      $(squareEl).removeClass();
-      $(squareEl).addClass(classToAdd);
+      $(squareEl).removeClass().addClass(classToAdd);
     }
   }
+};
 
+Controller.prototype.loadKeyStrokeListeners = function() {
+  var that = this;
+  $('html').keydown(function (event) {
+    switch(event.keyCode){
+      case 37:
+        that.game.snake.turn("west");
+        break;
+      case 38:
+        that.game.snake.turn("north");
+        break;
+      case 39:
+        that.game.snake.turn("east");
+        break;
+      case 40:
+        that.game.snake.turn("south");
+        break;
+      case 32:
+        that.stopLoop();
+        break;
+      case 13:
+        that.paused = false;
+        that.runLoop();
+        break;
+      default:
+        console.log(event.keyCode);
+        break;
+    }
+  });
 };
 
 $(document).ready(function() {
   controller = new Controller();
   controller.buildBoard();
+  controller.loadKeyStrokeListeners();
   controller.render();
+  controller.runLoop();
 });
